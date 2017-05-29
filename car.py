9cf6579b12da926771ad10f6114c5ab3a2d01fae
oliver <loliverhennigh101@gmail.com>
2017-05-28 19:40:07 -0500

    refactored code

---
+++ car.py
@@ -0,0 +1,100 @@
+
+import time
+
+import numpy as np
+import tensorflow as tf
+import math 
+import cv2
+
+from lb_solver import *
+
+import matplotlib.pyplot as plt 
+
+# video init
+fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v') 
+video = cv2.VideoWriter()
+
+shape = [64, 128]
+success = video.open('video.mov', fourcc, 4, (shape[1], shape[0]), True)
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_bool('test', True,
+                           """will save the state of the system for testing purposes""")
+
+def make_car_boundary(car_name="cars/car_001.png", shape=[256,1024], car_shape=(256,128)):
+  img = cv2.imread(car_name, 0)
+  img = cv2.flip(img, 1)
+  resized_img = cv2.resize(img, car_shape)
+  resized_img = -(resized_img/255.0).astype(int).astype(np.float32) + 1.0
+  resized_img = resized_img.reshape([1, car_shape[1], car_shape[0], 1])
+  boundary = np.zeros((1, shape[0], shape[1], 1), dtype=np.float32)
+  boundary[:, shape[0]-car_shape[1]:, 64:64+car_shape[0], :] = resized_img
+  boundary[:,0,:,:] = 1.0
+  return boundary
+
+def make_u_input(shape, value=0.001):
+  u = np.zeros((1,shape[0],1,9)) + 1.0/9.0
+  """
+  u[:,:,:,0] += value/6
+  u[:,:,:,1] += value/6
+  u[:,:,:,7] += value/6
+  u[:,:,:,3] -= value/6
+  u[:,:,:,4] -= value/6
+  u[:,:,:,5] -= value/6
+  """
+  u = u.astype(np.float32)
+  return u
+
+def run():
+  # constants
+  tau = 1.7
+  density = 1.0
+  input_vel = 0.00001
+
+  # make lattice state, boundary and input velocity
+  f = zeros_f(shape, density=density)
+  boundary = make_car_boundary(shape=shape, car_shape=(shape[1]/2, shape[0]/2))
+  boundary = tf.constant(boundary)
+  u_in = make_u_input(shape, value=input_vel)
+  u_in = tf.constant(u_in)
+ 
+  # construc solver 
+  step, u, f = lbm_step(f, boundary, u_in, tau=tau)
+
+
+  # init things
+  init = tf.global_variables_initializer()
+  # start sess
+  sess = tf.Session()
+  # init variables
+  sess.run(init)
+
+  # run steps
+  for i in range(50):
+    if i % 10 == 0:
+      f_r = f.eval(session=sess)
+      print(f_r[:,0:1,:,:])
+      u_r = u.eval(session=sess)
+      ux_r = u_r[0,:,:,0:1]
+      uy_r = u_r[0,:,:,1:2]
+      frame = np.square(ux_r) + np.square(uy_r)
+      frame = np.uint8(255 * frame/np.max(frame))
+      frame = cv2.applyColorMap(frame[:,:,0], 2)
+      video.write(frame)
+    t = time.time()
+    step.run(session=sess)
+    elapsed = time.time() - t
+    if i % 100 == 0:
+      print("step " + str(i))
+      print("time per step " + str(elapsed))
+
+def main(argv=None):  # pylint: disable=unused-argument
+  run()
+
+if __name__ == '__main__':
+  tf.app.run()
+
+
+
+
